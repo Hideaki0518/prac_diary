4a877b8ca5176e7fd992c615ecbe8d94f5c29ac3
hideaki <negohideaki@gmail.com>
2020-11-14 13:46:02 +0900

    implement form.

--- venv_diary/prac_diary/diary/views.py
+++ venv_diary/prac_diary/diary/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
+from .forms import InquiryForm
 
 
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+
+class InquiryViews(generic.FormView):
+    template_name = 'inquiry.html'
+    form_class = InquiryForm
